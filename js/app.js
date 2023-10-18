fd0973af79cbde809f8e19771bba13b068ddd997
CLAUDIU STEFAN <stefanclaudiu@programmer.net>
2023-10-18 01:31:34 +0100

    adding footer

--- js/app.js
+++ js/app.js
@@ -4,7 +4,15 @@ import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 createApp({
     data(){
         return{
-           
+           site:{
+            name: "After School Hub"
+           },
+           cart:{
+            counter: 0
+           },
+           copyright:{
+            year: 2023
+           },
         }
     }
   }).mount('#app')
